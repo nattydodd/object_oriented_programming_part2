2e9cda3fba2f72cc339af3e45d59ee0346f77b4e
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-12 18:56:23 -0400

    total price bug fixed

--- oop_exercise_2/item.rb
+++ oop_exercise_2/item.rb
@@ -21,30 +21,42 @@ class Item
 
 
   def add_tax
-     if ( @type == "food" || "medical" || "books" ) && ( @import == "false" )
-        puts "#{@name}: $#{@price}"
-        @@sales_tax
-        @@total_price + 0
-     elsif ( @type == "food" || "medical" || "books" ) && ( @import == "true" )
-        puts "#{@name}: $#{@price * 1.05}"
-        @@sales_tax += (@price * 0.05)
+     if ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == "false" )
+        puts "1 #{@name} : $#{@price.round(2)}"
+        # @@sales_tax
         @@total_price += @price
+     elsif ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == "true" )
+        puts "1 #{@name} : $#{ (@price * 1.05).round(2) }"
+        @@sales_tax += (@price * 0.05).round(2)
+        @@total_price += (@price * 1.05).round(2)
+     elsif ( @type != "food" || @type != "medical" || @type != "book" ) && ( @import == "true")
+        puts "1#{@name} : $#{ (@price * 1.15).round(2) }"
+        @@sales_tax += (@price * 0.15).round(2)
+        @@total_price += (@price * 1.15).round(2)
      else
-        puts "#{@name}: $#{@price * 1.10}"
-        @@sales_tax += (@price * 0.1)
-        @@total_price += @price
+        puts "1#{@name} : $#{ (@price * 1.10).round(2) }"
+        @@sales_tax += (@price * 0.10).round(2)
+        @@total_price += (@price * 1.10).round(2)
      end
   end
 
 
 end
 
+#Item List #1
 
+book = Item.new("Book", "book", 12.49, "false")
+music_cd = Item.new("Music CD", "music", 14.99, "false")
+chocolate_bar = Item.new("Chocolate Bar", "food", 0.85, "false")
+# chocolate = Item.new("Chocolate", "food", 1.99, "true")
+# tylenol = Item.new("Tylenol", "medical", 5.00, "false")
 
-chocolate = Item.new("Chocolate", "food", 1.99, "true")
-tylenol = Item.new("Tylenol", "medical", 5.00, "false")
+# chocolate.add_tax
+# tylenol.add_tax
+# #
+book.add_tax
+music_cd.add_tax
+chocolate_bar.add_tax
 
-chocolate.add_tax
-tylenol.add_tax
 puts "Sales Tax: $#{ Item.sales_tax }"
 puts "Total Bill: $#{ Item.total_price }"

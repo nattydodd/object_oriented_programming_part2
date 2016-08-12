25cd9671bc2747d6720ad1f6b057473c3c35dac7
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-12 19:21:41 -0400

    works but total is cumulative for all bills

--- oop_exercise_2/item.rb
+++ oop_exercise_2/item.rb
@@ -21,20 +21,20 @@ class Item
 
 
   def add_tax
-     if ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == "false" )
+     if ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == false )
         puts "1 #{@name} : $#{@price.round(2)}"
         # @@sales_tax
         @@total_price += @price
-     elsif ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == "true" )
+     elsif ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == true )
         puts "1 #{@name} : $#{ (@price * 1.05).round(2) }"
         @@sales_tax += (@price * 0.05).round(2)
         @@total_price += (@price * 1.05).round(2)
-     elsif ( @type != "food" || @type != "medical" || @type != "book" ) && ( @import == "true")
-        puts "1#{@name} : $#{ (@price * 1.15).round(2) }"
+     elsif ( @type != "food" || @type != "medical" || @type != "book" ) && ( @import == true)
+        puts "1 #{@name} : $#{ (@price * 1.15).round(2) }"
         @@sales_tax += (@price * 0.15).round(2)
         @@total_price += (@price * 1.15).round(2)
      else
-        puts "1#{@name} : $#{ (@price * 1.10).round(2) }"
+        puts "1 #{@name} : $#{ (@price * 1.10).round(2) }"
         @@sales_tax += (@price * 0.10).round(2)
         @@total_price += (@price * 1.10).round(2)
      end
@@ -45,18 +45,37 @@ end
 
 #Item List #1
 
-book = Item.new("Book", "book", 12.49, "false")
-music_cd = Item.new("Music CD", "music", 14.99, "false")
-chocolate_bar = Item.new("Chocolate Bar", "food", 0.85, "false")
-# chocolate = Item.new("Chocolate", "food", 1.99, "true")
-# tylenol = Item.new("Tylenol", "medical", 5.00, "false")
+book = Item.new("Book", "book", 12.49, false)
+music_cd = Item.new("Music CD", "music", 14.99, false)
+chocolate_bar = Item.new("Chocolate Bar", "food", 0.85, false)
 
-# chocolate.add_tax
-# tylenol.add_tax
-# #
 book.add_tax
 music_cd.add_tax
 chocolate_bar.add_tax
 
-puts "Sales Tax: $#{ Item.sales_tax }"
-puts "Total Bill: $#{ Item.total_price }"
+puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
+puts "Total Bill: $#{ Item.total_price.round(2) }"
+
+#Item List #2
+import_choc = Item.new("Imported Chocolate", "food", 10.00, true)
+import_perfume = Item.new("Imported Perfume", "perfume", 47.50, true)
+
+import_choc.add_tax
+import_perfume.add_tax
+
+puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
+puts "Total Bill: $#{ Item.total_price.round(2) }"
+
+#Item List #3
+import_perfume2 = Item.new("Imported Perfume", "perfume", 27.99, true)
+perfume = Item.new("Perfume", "perfume", 18.99, true)
+pills = Item.new("Headache Pills", "medical", 9.75, true)
+import_choc2 = Item.new("Imported Chocolate", "food", 11.25, true)
+
+import_perfume2.add_tax
+perfume.add_tax
+pills.add_tax
+import_choc2.add_tax
+
+puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
+puts "Total Bill: $#{ Item.total_price.round(2) }"

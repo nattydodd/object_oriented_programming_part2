dd68b269729f810c509b34b7ab04464389a9106b
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-12 16:32:34 -0400

    Total Bill and Sales Tax printing

--- oop_exercise_2/item.rb
+++ oop_exercise_2/item.rb
@@ -1,11 +1,15 @@
-# require_relative 'receipt'
 
 class Item
 
   @@total_price = 0
+  @@sales_tax = 0
 
   def self.total_price
     @@total_price
+  end
+
+  def self.sales_tax
+    @@sales_tax
   end
 
   def initialize(name, type, price, import)
@@ -19,15 +23,19 @@ class Item
   def add_tax
      if ( @type == "food" || "medical" || "books" ) && ( @import == "false" )
         puts "#{@name}: $#{@price}"
-        @@total_price += @price
+        @@sales_tax
+        @@total_price + 0
      elsif ( @type == "food" || "medical" || "books" ) && ( @import == "true" )
         puts "#{@name}: $#{@price * 1.05}"
+        @@sales_tax += (@price * 0.05)
         @@total_price += @price
      else
         puts "#{@name}: $#{@price * 1.10}"
+        @@sales_tax += (@price * 0.1)
         @@total_price += @price
      end
   end
+
 
 end
 
@@ -38,5 +46,5 @@ tylenol = Item.new("Tylenol", "medical", 5.00, "false")
 
 chocolate.add_tax
 tylenol.add_tax
-puts "Sales Tax: "
+puts "Sales Tax: $#{ Item.sales_tax }"
 puts "Total Bill: $#{ Item.total_price }"

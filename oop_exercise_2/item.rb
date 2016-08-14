9a520033fb8bfedf048229768fb5ebb09ba3fe13
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-14 00:01:33 -0400

    added class of receipt and everything is working

--- oop_exercise_2/item.rb
+++ oop_exercise_2/item.rb
@@ -1,5 +1,10 @@
+# require_relative 'receipt'
 
 class Item
+
+  attr_reader :name
+  attr_reader :price
+  attr_accessor :total_price
 
   @@total_price = 0
   @@sales_tax = 0
@@ -22,61 +27,78 @@ class Item
 
   def add_tax
      if ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == false )
-        puts "1 #{@name} : $#{@price.round(2)}"
-        # @@sales_tax
+        # puts "1 #{@name} : $#{@price.round(2)}"
+        @@sales_tax
         @@total_price += @price
+        @price.round(2)
      elsif ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == true )
-        puts "1 #{@name} : $#{ (@price * 1.05).round(2) }"
+        # puts "1 #{@name} : $#{ (@price * 1.05).round(2) }"
         @@sales_tax += (@price * 0.05).round(2)
         @@total_price += (@price * 1.05).round(2)
+        (@price * 1.05).round(2)
      elsif ( @type != "food" || @type != "medical" || @type != "book" ) && ( @import == true)
-        puts "1 #{@name} : $#{ (@price * 1.15).round(2) }"
+        # puts "1 #{@name} : $#{ (@price * 1.15).round(2) }"
         @@sales_tax += (@price * 0.15).round(2)
         @@total_price += (@price * 1.15).round(2)
+        (@price * 1.15).round(2)
      else
-        puts "1 #{@name} : $#{ (@price * 1.10).round(2) }"
+        # puts "1 #{@name} : $#{ (@price * 1.10).round(2) }"
         @@sales_tax += (@price * 0.10).round(2)
         @@total_price += (@price * 1.10).round(2)
+        (@price * 1.10).round(2)
      end
-     
+
   end
 
+  # def calc_sales_tax
+  #   if ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == false )
+  #      @@sales_tax
+  #      puts "Sales tax: #{ @@sales_tax }"
+  #   elsif ( @type == "food" || @type == "medical" || @type == "book" ) && ( @import == true )
+  #      puts "Sales tax: #{ @@sales_tax += (@price * 0.05).round(2) }"
+  #   elsif ( @type != "food" || @type != "medical" || @type != "book" ) && ( @import == true )
+  #      puts "Sales tax: #{ @@sales_tax += (@price * 0.15).round(2) }"
+  #   else
+  #      puts "Sales tax: #{@@sales_tax += (@price * 0.10).round(2) }"
+  #   end
+  # end
 
 end
 
 #Item List #1
+#*******************************************
+
 
 book = Item.new("Book", "book", 12.49, false)
 music_cd = Item.new("Music CD", "music", 14.99, false)
 chocolate_bar = Item.new("Chocolate Bar", "food", 0.85, false)
 
-book.add_tax
-music_cd.add_tax
-chocolate_bar.add_tax
 
-puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
-puts "Total Bill: $#{ Item.total_price.round(2) }"
+#Would like to make this universal, instead of having to call each item seperately
+item_list1 = {
+  book.name => book.add_tax,
+  music_cd.name => music_cd.add_tax,
+  chocolate_bar.name => chocolate_bar.add_tax
+}
 
-#Item List #2
-import_choc = Item.new("Imported Chocolate", "food", 10.00, true)
-import_perfume = Item.new("Imported Perfume", "perfume", 47.50, true)
 
-import_choc.add_tax
-import_perfume.add_tax
+class Receipt
 
-puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
-puts "Total Bill: $#{ Item.total_price.round(2) }"
+  def initialize(item_list)
+    @item_list = item_list
+  end
 
-#Item List #3
-import_perfume2 = Item.new("Imported Perfume", "perfume", 27.99, true)
-perfume = Item.new("Perfume", "perfume", 18.99, false)
-pills = Item.new("Headache Pills", "medical", 9.75, false)
-import_choc2 = Item.new("Imported Chocolate", "food", 11.25, true)
+  def print_items
+    @item_list.each do |item, price|
+    puts "1 #{item} : $#{price}"
+    end
+  end
 
-import_perfume2.add_tax
-perfume.add_tax
-pills.add_tax
-import_choc2.add_tax
+end
 
-puts "Sales Tax: $#{ Item.sales_tax.round(2) }"
-puts "Total Bill: $#{ Item.total_price.round(2) }"
+receipt1 = Receipt.new(item_list1)
+
+receipt1.print_items
+
+puts "Sales Tax: $#{ Item.sales_tax }"
+puts "Total Bill: $#{ Item.total_price }"
--- oop_exercise_2/receipt.rb
+++ oop_exercise_2/receipt.rb
@@ -1,11 +1,30 @@
 # require_relative 'item'
 #
-# class Receipt < Item
+# class Receipt
 #
-#   # def post_tax_item
-#   #   puts "1 #{@name} at #{@price}"
-#   # end
+#   def initialize(item_list)
+#     @item_list = item_list
+#   end
+#
+#   def print_items
+#     @item_list.each do |item, price|
+#     puts "1 #{item} : $#{price}"
+#     end
+#   end
+#
+#
+#   def print_sales_tax
+#     puts sales_total
+#     puts bill_total
+#   end
 #
 #
 # end
 #
+# receipt1 = Receipt.new(item_list_1)
+#
+# puts receipt1.print_items
+
+# item_list_2 = Receipt.new(import_choc, import_perfume)
+#
+# item_list_3 = Receipt.new(import_perfume2, perfume, pills, import_choc2)
